d867866f482f3d016ea0282ab8296c97eee73816
elguet5 <carloxhunter@gmail.com>
2022-07-08 18:41:24 -0400

    seems all ok IF only IF the matrix already come in canonical form

--- task3/markov/solution.py
+++ task3/markov/solution.py
@@ -1,27 +1,27 @@
-
-
-def zeros_matrix(rows, cols):
-    A = []
-    for i in range(rows):
-        A.append([])
-        for j in range(cols):
-            A[-1].append(myFrac(0,1))
-    return A
-
-def copy_matrix(M):
-    rows = len(M)
-    cols = len(M[0])
-    MC = zeros_matrix(rows, cols)
-    for i in range(rows):
-        for j in range(rows):
-            MC[i][j] = M[i][j]
-
+def identity(N):
+    m = []
+    for row in range(0, N):
+        temp = []
+        for col in range(0, N):
+            # Here end is used to stay in same line
+            if (row == col):
+                temp.append(myFrac(1,1))
+            else:
+                temp.append(myFrac(0,1))
+        m.append(temp)
+    return m
+def copy_matrix2(m):
+    res= []
+    for i in m:
+        temp = []
+        for j in i:
+            temp.append(j)
+        res.append(temp)
+    return res
 def gcd(x, y):
     while y != 0:
         (x, y) = (y, x % y)
     return x
-
-
 class myFrac:
     def __init__(self,up,down):
         if down == 0:
@@ -29,12 +29,16 @@ class myFrac:
         self.up = up
         self.down = down
     def __add__(self, other):
-        if other.up == 0:
+        if other.up == 0 or other  == 0:
             return self
         up = self.up * other.down + other.up * self.down
         do =  self.down * other.down
         if up == do :return myFrac(1,1)
         return myFrac(up,do)
+    def __radd__(self, other):
+        if other  == 0:
+            return self
+        return self.__add__(other)
     def __sub__(self, other):
         if other.up == 0:
             return self
@@ -70,6 +74,8 @@ class myFrac:
         if self.up == self.down:
             return str(1)
         return str(self.up)+"/"+str(self.down)
+    def __neg__(self):
+        return myFrac(-1 * self.up,self.down)
     def getInverse(self):
         return myFrac(self.down, self.up)
     def setInverse(self):
@@ -86,92 +92,13 @@ class myFrac:
     def getsimplify(self):
         gcdd = gcd(self.up, self.down)
         return myFrac(self.up / gcdd, self.down / gcdd)
-    
-
-
-
-
-def solution(m):
-    for i in m:
-        print (i)
-    return 1
-
-def prepareMatrix(m):
-    #dest = {}
-    absStates= []
-    mults=[]
-    for i, vali in enumerate(m):
-        rowSum = sum(vali)
-        mults.append(rowSum)
-        #print(rowSum, vali)
-        #for j,valj in enumerate(vali):
-        #    dest[(i,j)] = valj != 0   
-        if rowSum == 0:
-            vali[i] = 1
-            absStates.append(i)         
-    return m, absStates, mults
-
 def defineSubM1(m, firstAbs):
-    #assumption they come in cnaonical form
     mQ = []
     mR = []
-    #mO = [] no interesa (aun)
-    #mI = [] no interesa (aun)
     for vali in m[:firstAbs]:
         mQ.append(vali[:firstAbs])
         mR.append(vali[firstAbs:])
     return mQ, mR
-
-def defineSubM2(m):
-    #they come in cnaonical form
-    mQ = []
-    mR = []
-    #mO = [] no interesa (aun)
-    #mI = [] no interesa (aun)
-    for i, vali in enumerate(m):
-        rowSum = sum(vali)
-        if rowSum == 0:
-            break
-    #print(i)
-    for j, valj in enumerate(m[:i]):
-        #print(valj)
-        mQ.append(valj[:i])
-        mR.append(valj[i:])
-    return mQ, mR
-        
-
-M = [[0,2,1,0,0],
-    [0,0,0,3,4],
-    [0,0,0,0,0],
-    [0,0,0,0,0],
-    [0,0,0,0,0]]
-#a= solution(M)
-#m2, absStates, mults = prepareMatrix(M)
-
-#for kiko in m2:
-#    print(kiko)
-#print(absStates)
-#print(mults)
-#print(M)
-
-mq, mr = defineSubM2(M)
-#print(mq)
-#print(mr)
-
-
-#print(myFrac(1,0)) # <-- Raise error -->
-#print(myFrac(0,1))
-#print(myFrac(1,1)+myFrac(0,1))
-#print(myFrac(0,1)+myFrac(1,1))
-#print(myFrac(1,1)-myFrac(0,1))
-#print(myFrac(0,1)-myFrac(1,1))
-#print(myFrac(1,1)*myFrac(0,1))
-#print(myFrac(0,1)*myFrac(1,1))
-#print(myFrac(1,1)/myFrac(0,1)) #<-- Raise error -->
-#print(myFrac(0,1)/myFrac(1,1))
-#print(myFrac(1,2)*myFrac(2,1))
-#print(myFrac(1,2)*myFrac(1,2).getInverse())
-
 def prepMatrix3(m):
     res = []
     absStates= []
@@ -187,20 +114,9 @@ def prepMatrix3(m):
                 temp.append(myFrac(item,1))
         res.append(temp)
     return res, absStates
-
 def printMatrix(m):
     for row in m:
         print(row)
-
-Mown, absStates = prepMatrix3(M)
-
-#printMatrix(Mown)
-
-mQ, mR = defineSubM1(Mown, absStates[0])
-#print(mQ)
-#print(mR)
-
-
 def ISubsQ(M):
     nrows = len(M)
     ncols = len(M[0])
@@ -210,53 +126,12 @@ def ISubsQ(M):
         for j, valj in enumerate(vali):
             if (i == j):
                 M[i][j] = myFrac(1,1) - valj
-    return M
-
-
-#print(ISubsQ(mQ))
-#print(myFrac(0,1) - myFrac(1,1))
-
-def identity(N):
-    m = []
-    for row in range(0, N):
-        temp = []
-        for col in range(0, N):
-            # Here end is used to stay in same line
-            if (row == col):
-                temp.append(myFrac(1,1))
             else:
-                temp.append(myFrac(0,1))
-        m.append(temp)
-    return m
-
-def copy_matrix2(m):
-    res= []
-    for i in m:
-        temp = []
-        for j in i:
-            temp.append(j)
-        res.append(temp)
-    return res
-
-def oppositeSigns(x, y):
-    val = (x ^ y) < 0
-    print('signcheck',x,y, val)
-    return ((x ^ y) < 0);
-
-def printSideBySide(m1,m2):
-    #temp = []
-    for i in range(len(m1)):
-        temp = []
-        temp.extend(m1[i])
-        temp.append('|')
-        temp.extend(m2[i])
-        print(temp)    
-
+                M[i][j] = - valj  
+    return M
 def gaussInvert(m):
     #this should be square matrix in order to be invertible
     nrows = len(m)
-    #Im = identity(nrows)
-    #mres = copy_matrix2(m)
     extendedM = []
     for el in range(nrows):
         temp = m[el]
@@ -266,100 +141,97 @@ def gaussInvert(m):
                 I = myFrac(1,1)
             temp.append(I)
         extendedM.append(temp)
-    
-    #c=0
-    print('gauss')
     for col in range(nrows):
-        #print('new col!',col)
-        #find and set pivot
-        #print("finding pivot..")
-        #check if element in [c][c] --> diagonal != 0
         val = extendedM[col][col]
         pivot_idx = col
-        if val.up != 0: pass #print('pivot in CC')
+        if val.up != 0: pass 
         else: #take the first not null and move to first pos
-            #print('pivot not in CC')
             for j in range(col,nrows):
                 if (j > col):
                     if (extendedM[j][col] != 0):
-                        #print('pivot found in pos ',j,col)
                         pivot_idx=j
-                        break
-                    #print(j,mres[j][col])
-            #print('after loop')
-        #print('pivot!', extendedM[pivot_idx])
-        
-        
+                        break   
         if pivot_idx != col:
             #intercharge cc rows with pivot pos so we can have pivots in 0,0; 1,1; 2,2;... and so
-            #print('intercambiando filas al pivote')
             pivot = extendedM[pivot_idx]
             cc_row = extendedM[col]
             extendedM[col] = pivot
             extendedM[pivot_idx] = cc_row
-
-
-
-        #printMatrix(extendedM)
-            #now pivot is on CC
+        #now pivot is on CC
         val = extendedM[col][col]
         if val != 1:
             #resize pivot to 1
-            #print('dejando pivote en 1 (positivo')
             invertedVal = val.getInverse()
-            #print('inverrtedval',invertedVal.up * invertedVal.down)
             extendedM[col] = [k * invertedVal for k in extendedM[col]]
-
-
         #sum to down
         for j in range(nrows):
             if( j != col):
-                #print('sumando hacia abajo')
                 sumRow = [extendedM[j][col]*myFrac(-1,1)* k for k in extendedM[col]]
-                extendedM[j] =  [x + y for x, y in zip(extendedM[j], sumRow)]
-
-                
-
-            
+                extendedM[j] =  [x + y for x, y in zip(extendedM[j], sumRow)]       
     res = []
     for ijk in extendedM:
         res.append(ijk[nrows:])
-    #printMatrix(extendedM)
-
     for ey in res:
         for eyy in ey:
             eyy.simplySelf()
-        
-        
     return res
-            
-
 def getIn(m):
     nrows = len(m)
     ncols = len(m)
     if nrows != ncols:
-        return m
-    
-    #print('Q')
-    #printMatrix(m)
-    #print('I')
-    #printMatrix(Im)
-    #print('I - Q')
-    iq = ISubsQ(m)
-    #printMatrix(iq)
-    #print('jiro')
-    testM = [[myFrac(1,1), myFrac(2,1), myFrac(-1,1)],
+        raise Exception('Should be square matrix')
+    #maybe some further checks someday    
+    inverted = gaussInvert(m)
+    return inverted
+def matmult(a,b):
+    zip_b = zip(*b)
+    # uncomment next line if python 3 : 
+    # zip_b = list(zip_b)
+    return [[sum(ele_a*ele_b for ele_a, ele_b in zip(row_a, col_b)) 
+             for col_b in zip_b] for row_a in a]   
+
+
+
+M = [[0,2,1,0,0],
+    [0,0,0,3,4],
+    [0,0,0,0,0],
+    [0,0,0,0,0],
+    [0,0,0,0,0]]
+
+testM = [[myFrac(1,1), myFrac(2,1), myFrac(-1,1)],
             [myFrac(2,1), myFrac(7,1), myFrac(2,1)],
             [myFrac(20,1),myFrac(2,1),myFrac(0,1)]]
-    inverted = gaussInvert(testM)
-    printMatrix(inverted)
+
+Mown, absStates = prepMatrix3(M)
+print('matrix prepared')
+printMatrix(Mown)
+mQ, mR = defineSubM1(Mown, absStates[0])
+print('Q')
+printMatrix(mQ)
+iq = ISubsQ(mQ)
+print('I-Q')
+printMatrix(iq)
+mN = getIn(iq)
+print('N')
+printMatrix(mN)
+print('B')
+mB = matmult(mN, mR)
+for ey in mB:
+        for eyy in ey:
+            eyy.simplySelf()
+printMatrix(mB)
 
 
-getIn(mQ)
+       
 
 
 
-#print(myFrac(11,13).getsimplify())       
+
+def solution(m):
+    for i in m:
+        print (i)
+    return 1
+   
 
 
 
